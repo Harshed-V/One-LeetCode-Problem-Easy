1adc0b250984d86ff4f4d81f0be923823cdaaf9e
HARSHED V <158579162+Harshed-V@users.noreply.github.com>
2026-05-02 10:38:45 +0530

    Remove unnecessary whitespace in isSymmetric method

--- 101SymmetricTree.java
+++ 101SymmetricTree.java
@@ -2,8 +2,7 @@
 class Solution {
     public boolean isSymmetric(TreeNode root) {
         if(root == null) return true;
-        return isMirror(root.left, root.right);        
-         
+        return isMirror(root.left, root.right);      
     }public boolean isMirror(TreeNode left, TreeNode right){
         if(left == null && right == null) return true;
         if(left == null || right == null) return false;
