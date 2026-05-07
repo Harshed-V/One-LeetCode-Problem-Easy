46631bc4d5df82568fa61dab10e0694d307f2c88
HARSHED V <158579162+Harshed-V@users.noreply.github.com>
2026-05-07 15:23:51 +0530

    Refactor isMirror method for better readability

--- 101SymmetricTree.java
+++ 101SymmetricTree.java
@@ -4,9 +4,12 @@ class Solution {
         if(root == null) return true;
         return isMirror(root.left, root.right);      
     }public boolean isMirror(TreeNode left, TreeNode right){
-        if(left == null && right == null) return true;
-        if(left == null || right == null) return false;
-        if(left.val != right.val) return false;
+        if(left == null && right == null)
+            return true;
+        if(left == null || right == null)
+            return false;
+        if(left.val != right.val) 
+            return false;
         return isMirror(left.left, right.right) && isMirror(left.right, right.left);
     }
 }
